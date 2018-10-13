3fee7c24e2232ffca78cc73d26ea9dbf99d1dfc2
forana <alex@alexforan.com>
2018-10-13 12:35:02 -0500

    pi stuff done

--- pi/tatertimer.py
+++ pi/tatertimer.py
@@ -1,4 +1,9 @@
 import time
+import math
+import sys
+import threading
+import subprocess
+
 import RPi.GPIO as GPIO
 
 import Adafruit_GPIO.SPI as SPI
@@ -8,7 +13,19 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-import subprocess
+led_threshold = 30 # seconds before LED lights up
+
+last_time = time.time() # global tracking the last time tater went out
+time_since_string = ""
+
+wifi_connected = False
+
+def log(message):
+    sys.stdout.write(message)
+    sys.stdout.write("\n")
+    sys.stdout.flush()
+
+log("tater timer starting")
 
 RST = None
 disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
@@ -21,55 +38,90 @@ width = disp.width
 height = disp.height
 image = Image.new('1', (width, height))
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(24, GPIO.OUT)
-toggle = True
-print("press the button to toggle the light")
-last_read = GPIO.LOW
-
-# Get drawing object to draw on image.
 draw = ImageDraw.Draw(image)
-
-# Draw a black filled box to clear the image.
 draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-# Draw some shapes.
-# First define some constants to allow easy resizing of shapes.
 padding = -2
 top = padding
 bottom = height-padding
-# Move left to right keeping track of the current x position for drawing shapes.
+
 x = 0
 
 # Load font.
 # font = ImageFont.load_default()
-font = ImageFont.truetype(font = "font.ttf", size = 16)
+small_font = ImageFont.truetype(font = "font.ttf", size = 16)
+large_font = ImageFont.truetype(font = "font.ttf", size = 48)
 
-try:
+log("display initialized")
+
+GPIO.setmode(GPIO.BCM)
+# active-low button
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# led w/ resistor
+GPIO.setup(24, GPIO.OUT)
+
+log("GPIO initialized")
+
+def button_check():
+    global last_time
+    last_read = GPIO.LOW
     while True:
+        # read the button with some craptastic debouncing
         current_read = GPIO.input(23)
         if current_read != last_read and current_read == GPIO.LOW:
-            toggle = not toggle
+            # then the button was pressed, and we reset the timer
+            last_time = time.time()
+            log("time reset")
         last_read = current_read
-        GPIO.output(24, GPIO.HIGH if toggle else GPIO.LOW)
+        time.sleep(0.01)
 
-        draw.rectangle((0,0,width,height), outline=0, fill=0)
-    
-        draw.text((x, top), "tater timer :)", font=font, fill=255)
-    
-        cmd = "hostname -I | cut -d\' \' -f1"
-        IP = subprocess.check_output(cmd, shell = True)
-        draw.text((x, top + 16), "IP: " + str(IP),  font=font, fill=255)
-    
-        draw.text((x, top + 32), "time: " + time.strftime("%I:%M:%S %p"), font=font, fill=255)
+def time_control():
+    global time_since_string
+    while True:
+        # do time math like an idiot
+        time_since = time.time() - last_time
+        h = math.floor(time_since/3600)
+        m = math.floor(time_since/60)
+        s = time_since%60
+        time_since_string = "%02d:%02d:%02d" % (h, m, s)
+
+        # if the time's past the threshold, light it up
+        GPIO.output(24, GPIO.HIGH if led_threshold <= time_since else GPIO.LOW)
         
-        draw.text((x, top + 48), "button: " + ("up" if current_read else "down"), font=font, fill=255)
+        time.sleep(1)
+
+def wifi_check():
+    global wifi_connected
+    # check wifi state real hackily
+    try:
+        subprocess.check_output("iwconfig 2>&1 | grep ESSID", shell = True)
+        wifi_connected = True
+    except subprocess.CalledProcessError as e:
+        wifi_connected = False
+
+def screen_control():
+    while True:
+        # new blank frame
+        draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+        if time.time() - last_time >= led_threshold:
+            draw.text((x, top + 0), "take the boy outside!", font=small_font, fill=255)
+
+        draw.text((x, top + 16), time_since_string, font=large_font, fill=255)
+        draw.text((x, top + 52), "wifi: " + ("yes :)" if wifi_connected else "no :( :( :("), font=small_font, fill=255)
 
         disp.image(image)
         disp.display()
-        time.sleep(.01)
-finally:
-    disp.clear()
-    GPIO.cleanup()
 
+        time.sleep(0.3)
+
+threading.Thread(target = button_check).start()
+threading.Thread(target = time_control).start()
+threading.Thread(target = wifi_check).start()
+threading.Thread(target = screen_control).start()
+
+log("tater timer running")
+
+# internals take care of these automatically, apparently?
+#     disp.clear()
+#     GPIO.cleanup()
